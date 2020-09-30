10ccb74f754128c2ecfc0bcd256cff6cff921fd3
nafkong <2500746217@qq.com>
2020-09-30 16:25:10 +0800

    修改注释

--- exc1.cpp
+++ exc1.cpp
@@ -1,18 +1,18 @@
-//#include<iostream>
-//#include <opencv2/opencv.hpp>
-//#include<opencv2/highgui.hpp>
-//using namespace std;
-//using namespace cv;
-//int main() {
-//	Mat src_color = imread("source.png");
-//	vector<Mat> channels;
-//	split(src_color, channels);
-//	Mat B = channels.at(0);
-//	Mat G = channels.at(1);
-//	Mat R = channels.at(2);
-//	imshow("red", R);
-//	imshow("blue", B);
-//	imshow("green", G);
-//	imshow("original Mat", src_color);
-//	waitKey(0);
-//}
+#include<iostream>
+#include <opencv2/opencv.hpp>
+#include<opencv2/highgui.hpp>
+using namespace std;
+using namespace cv;
+int main() {
+	Mat src_color = imread("source.png");
+	vector<Mat> channels;
+	split(src_color, channels);
+	Mat B = channels.at(0);
+	Mat G = channels.at(1);
+	Mat R = channels.at(2);
+	imshow("red", R);
+	imshow("blue", B);
+	imshow("green", G);
+	imshow("original Mat", src_color);
+	waitKey(0);
+}
